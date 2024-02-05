fdd52c1580993efed1de8f0146bb89c5113a93f5
0793rebecca <rebeccafrempongmensah@gmail.com>
2024-02-05 15:25:28 -0800

    Modify

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,9 +1,15 @@
-function taskFirst() {
-  const variable1 = "First Variable";
-  const variable2 = 42;
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
 }
 
-function taskNext() {
-  let variable1 = "Next Variable";
-  let variable2 = 100;
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
 }
